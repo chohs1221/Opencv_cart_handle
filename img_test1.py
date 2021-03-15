674b082377b4d769810ae979cf4a9ade33a09463
chohs1221 <hscho1221@naver.com>
2021-03-15 19:40:21 +0900

    add img_test.py,

---
+++ img_test1.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+
+# 블러링
+def Bluring(img, kernel_size):
+    img_blur = cv2.blur(img, (kernel_size, kernel_size), anchor=(-1, -1), borderType=cv2.BORDER_DEFAULT)
+    #cv2.imshow('blur', img_blur)
+    return img_blur
+# 그레이스케일
+def Grayscale(img, thresh):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret, binary = cv2.threshold(gray, thresh, 255, cv2.THRESH_BINARY)
+    binary = cv2.bitwise_not(binary)
+    #cv2.imshow('binary', binary)
+    return binary
+# 컨투어
+def draw_Contours(img, height, width, channel):
+    contours, _ = cv2.findContours(
+        img, 
+        mode=cv2.RETR_LIST, 
+        method=cv2.CHAIN_APPROX_SIMPLE
+    )
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+    cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
+    #cv2.imshow('contours', temp_result)
+    return contours, temp_result
+# 컨투어 박스
+def draw_ContourBox(img, contours, min_width, min_ratio, height, width, channel):
+    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+    contours_dict = []
+    for contour in contours:
+        rect = cv2.minAreaRect(contour)
+        if rect[1][0] > min_width and (rect[1][0] // rect[1][1]) > min_ratio:
+            box = cv2.boxPoints(rect)
+            box = np.int0(box) 
+            cv2.drawContours(temp_result, [box], -1, (0, 255, 0), 2)
+            print("==========================================================")
+            print("(x, y) = ({0})\n(width, height) = {1}\n(angle) = {2}".format(rect[0], rect[1], rect[2]))
+            print(box)
+            
+    #cv2.imshow('result', temp_result)
+    return temp_result
+
+if __name__ == "__main__":
+    img = cv2.imread('5.jpg')
+    height, width, channel = img.shape
+    cv2.imshow('original', img)
+
+    img_blur = Bluring(img, 15)
+    cv2.imshow('img_blur', img_blur)
+    img_binary = Grayscale(img_blur, 170)
+    cv2.imshow('img_binary', img_binary)
+    contours, img_contour = draw_Contours(img_binary, height, width, channel)
+    cv2.imshow('contours', img_contour)
+    img_contourBox = draw_ContourBox(img_contour, contours, 10, 2, height, width, channel)
+    cv2.imshow('img_contourBox', img_contourBox)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- video_test1.py
+++ video_test1.py
@@ -1,5 +1,5 @@
-import cv2
-import numpy as np
+import img_test1
+from img_test1 import cv2, np
 
 capture = cv2.VideoCapture(1)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -10,33 +10,16 @@ while cv2.waitKey(33) < 0:
     cv2.imshow("VideoFrame", frame)
     height, width, channel = frame.shape
 
-    # 블러링
-    img_blur = cv2.blur(frame, (9, 9), anchor=(-1, -1), borderType=cv2.BORDER_DEFAULT)
-    # 그레이스케일
-    gray = cv2.cvtColor(img_blur, cv2.COLOR_RGB2GRAY)
-    ret, binary = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY)
-    binary = cv2.bitwise_not(binary)
-    # 컨투어
-    contours, _ = cv2.findContours(
-        binary, 
-        mode=cv2.RETR_LIST, 
-        method=cv2.CHAIN_APPROX_SIMPLE
-    )
-    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-    cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
-    cv2.imshow('contours', temp_result)
-    # 컨투어 박스
-    temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+    img_blur = img_test1.Bluring(frame, 15)
+    cv2.imshow('img_blur', img_blur)
+    img_binary = img_test1.Grayscale(img_blur, 170)
+    cv2.imshow('img_binary', img_binary)
+    contours, img_contour = img_test1.draw_Contours(img_binary, height, width, channel)
+    cv2.imshow('contours', img_contour)
+    img_contourBox = img_test1.draw_ContourBox(img_contour, contours, 100, 1, height, width, channel)
+    cv2.imshow('img_contourBox', img_contourBox)
 
-    contours_dict = []
-
-    for contour in contours:
-        rect = cv2.minAreaRect(contour)
-        if rect[1][0] > 250 and (rect[1][0] // rect[1][1]) > 5:
-            box = cv2.boxPoints(rect)
-            box = np.int0(box) 
-            cv2.drawContours(temp_result, [box], -1, (0, 255, 0), 2)
-    cv2.imshow('result', temp_result)
+    
 
 
 capture.release()
