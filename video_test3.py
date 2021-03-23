c3de3a675d1819d22cd49fa6418b48902e839e4f
chohs1221 <hscho1221@naver.com>
2021-03-23 22:00:35 +0900

    put trackbar

--- video_test3.py
+++ video_test3.py
@@ -5,6 +5,25 @@ capture = cv2.VideoCapture(1)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+def onChange(pos):
+    pass
+
+cv2.namedWindow("img_contourBox")
+
+cv2.createTrackbar("h_min", "img_contourBox", 0, 179, onChange)
+cv2.createTrackbar("h_max", "img_contourBox", 0, 179, onChange)
+cv2.createTrackbar("s_min", "img_contourBox", 0, 255, onChange)
+cv2.createTrackbar("s_max", "img_contourBox", 0, 255, onChange)
+cv2.createTrackbar("v_min", "img_contourBox", 0, 255, onChange)
+cv2.createTrackbar("v_max", "img_contourBox", 0, 255, onChange)
+
+cv2.setTrackbarPos("h_min", "img_contourBox", 7)
+cv2.setTrackbarPos("h_max", "img_contourBox", 30)
+cv2.setTrackbarPos("s_min", "img_contourBox", 20)
+cv2.setTrackbarPos("s_max", "img_contourBox", 140)
+cv2.setTrackbarPos("v_min", "img_contourBox", 190)
+cv2.setTrackbarPos("v_max", "img_contourBox", 250)
+
 while cv2.waitKey(33) != ord('q'):
     ret, frame = capture.read()
     cv2.imshow("VideoFrame", frame)
@@ -12,6 +31,13 @@ while cv2.waitKey(33) != ord('q'):
 
     low = [7, 20, 190]
     high = [30, 140, 250]
+    low[0] = cv2.getTrackbarPos("h_min", "img_contourBox")
+    high[0] = cv2.getTrackbarPos("h_max", "img_contourBox")
+    low[1] = cv2.getTrackbarPos("s_min", "img_contourBox")
+    high[1] = cv2.getTrackbarPos("s_max", "img_contourBox")
+    low[2] = cv2.getTrackbarPos("v_min", "img_contourBox")
+    high[2] = cv2.getTrackbarPos("v_max", "img_contourBox")
+
     img_masked = mask(frame, low, high)
     cv2.imshow("img_masked", img_masked)
 
@@ -26,6 +52,9 @@ while cv2.waitKey(33) != ord('q'):
     cv2.putText(img_contourBox, "s: ("+str(low[1])+", "+str(high[1])+")", (10, 25), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
     cv2.putText(img_contourBox, "v: ("+str(low[2])+", "+str(high[2])+")", (10, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
     cv2.imshow('img_contourBox', img_contourBox)
+
+
+
 
 capture.release()
 cv2.destroyAllWindows()
